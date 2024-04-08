811031e6edf028a7be92c6e31e006f65c366c5e6
KendiAstro23 <litalakendy975@gmail.com>
2024-04-08 14:57:29 -0700

    Javascript is amazing

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
 
-if (process.argv.length === 2) {
-	console.log('No argument');
-} else if (process.argv.length === 3) {
-	console.log('Argument found');
+const process = require('process');
+
+const args = process.argv;
+
+if (args.length === 2) {
+  console.log('No argument');
+} else if (args.length === 3) {
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
